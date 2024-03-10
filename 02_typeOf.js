bb62994613e22637ad87e08c2357e5ef278ecfb4
sushantjedhe9893 <sushantjedhe9893@gmail.com>
2024-03-11 01:30:21 +0530

    Applied datatypes to the variable
    Applied datatypes to the variable

---
+++ 02_typeOf.js
@@ -0,0 +1,43 @@
+
+console.log("============= Step 1===============");
+
+var bankName;
+var typeOf = typeof bankName;
+console.log("Value of bank name before initilization :",bankName);
+console.log("DataType of bankName before initilization :",typeOf);
+bankName = "SBI bankName";
+console.log("Value of bank name after initilization :",bankName);
+var Typeof = typeof bankName;
+console.log("DataType of bankName after initilization :",Typeof);
+
+console.log("============= Step 2 =================");
+
+var marks = '90%';
+var percentage = typeof marks;
+console.log("Value of Marks is :",marks);
+console.log("Data types of percentage is :",percentage);
+
+console.log("================== Step 3 =====================");
+
+var isWorking;
+console.log("Value of isWorking before initilization is :",isWorking);
+var Typeof = typeof isWorking;
+console.log("Datatype of isWorking Before initilization is :",Typeof);
+isWorking = true;
+console.log("Value of isWorking after initilization is :",isWorking);
+var Typeof = typeof isWorking;
+console.log("Datatype of isWorking after initilization is :",Typeof);
+
+console.log("================ Step 4 ===============================");
+
+var totalCount = "Hundred and Seven";
+var Typeof = typeof totalCount;
+console.log("Value of totalCount before update is :",totalCount);
+console.log("DataType of totalCount before update is :",Typeof);
+
+console.log("--------------------------------------------------------------");
+
+var totalCount = 107;
+var Typeof = typeof totalCount;
+console.log("Value of totalCount after update is :",totalCount);
+console.log("DataType of totalCount after update is :",Typeof);
