3a926e18d68182f0668f4865e600a0fb3e278f39
sushantjedhe9893 <sushantjedhe9893@gmail.com>
2024-03-20 17:00:02 +0530

    Applied For loop Assginment
    Applied For loop Assginment

---
+++ 06_for_Assginment1.js
@@ -0,0 +1,40 @@
+console.log(`----- Numbers from 5 to 15 incrementing by 1 -----`);
+for (let index = 5; index <=15; index++) {
+    console.log(index);
+}
+
+console.log(`-----Numbers from 50 to 40 decrementing by 1 -----`);
+
+for (let index = 50; index >=40; index--) {
+    console.log(index);
+}
+
+console.log("------ First 15 odd numbers ------");
+
+for (let index = 1; index <=29; index=index+2) {
+   console.log(index);
+}
+
+console.log("------ First 10 even numbers ------");
+
+for (let index = 0; index <=18; index=index+2) {
+    console.log(index);
+}
+
+console.log("------ Table of 5 ------");
+
+for (let index = 5; index <=50; index=index+5) {
+    console.log(index);
+}
+
+console.log("------ Table of 10 ------");
+
+for (let index = 10; index <=100; index=index+10) {
+    console.log(index);
+}
+
+console.log("------ Reverese order of Table 10 ------");
+
+for (let index = 100; index >=10; index=index-10) {
+    console.log(index);
+}
