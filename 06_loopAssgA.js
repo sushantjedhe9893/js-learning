7474a2f37f238a9e4aa715174f48a292de14b2c4
sushantjedhe9893 <sushantjedhe9893@gmail.com>
2024-03-21 01:25:22 +0530

    Applied Scope Assignment
    Applied Scope Assignment

---
+++ 06_loopAssgA.js
@@ -0,0 +1,38 @@
+console.log(`1. Count the total number of vowels including small & capital using for loop`);
+
+const word = "I am very good IT developer";
+const vowels = "aeiou";
+let vowelsCount = 0;
+for (let index = 0; index < word.length; index++) {
+    let char = word.charAt(index);
+    let lowerCase = char.toLowerCase();
+    if (vowels.includes(lowerCase)) {
+        vowelsCount++;
+    }
+    
+}
+console.log(`-> The count is :${vowelsCount}`);
+
+console.log(`2. Write a fn to get sum of cube of numbers from 1 to 5`);
+
+let sum = 0;
+for (let index = 0; index <=5; index++) {
+    sum = sum + index * index * index;
+    
+}
+console.log(`-> ${sum}`);
+
+console.log(`3. Odd position chars of string1 & string2`);
+
+function oddPositionedChars(word){
+for (let index = 0; index < word.length; index++) {
+    const element = word.charAt(index);
+    if(index%2!=0 && element != " "){
+        console.log(element);
+    }
+    
+}
+
+}
+oddPositionedChars("Hard work always pays back")
+oddPositionedChars("Soon i will be  UI IT Champ")
